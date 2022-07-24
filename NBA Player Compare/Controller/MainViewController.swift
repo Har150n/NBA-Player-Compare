67402328073216cad6ff2d02013cae79bdf1b04e
Harrison Shu <hshu02@calpoly.edu>
2022-07-24 10:30:12 +0100

    Added second player to compare

--- NBA Player Compare/Controller/MainViewController.swift
+++ NBA Player Compare/Controller/MainViewController.swift
@@ -8,22 +8,31 @@
 import UIKit
 
 class MainViewController: UIViewController, PlayerDelegate, UITextFieldDelegate {
+    
 
     @IBOutlet weak var playerOnePointsLabel: UILabel!
     @IBOutlet weak var playerOneAssistsLabel: UILabel!
     @IBOutlet weak var playerOneReboundsLabel: UILabel!
     @IBOutlet weak var playerOneBlocksLabel: UILabel!
     @IBOutlet weak var playerOneSearchTextField: UITextField!
+    
+    @IBOutlet weak var playerTwoPointsLabel: UILabel!
+    @IBOutlet weak var playerTwoAssistsLabel: UILabel!
+    @IBOutlet weak var playerTwoReboundsLabel: UILabel!
+    @IBOutlet weak var playerTwoBlocksLabel: UILabel!
+    @IBOutlet weak var playerTwoSearchTextField: UITextField!
+    
+    let playerDict = PlayerDict()
+    let season = Database()
     override func viewDidLoad() {
-        let playerDict = PlayerDict()
-        super.viewDidLoad()
-        let season = Database()
-        season.getPlayer(id: String(playerDict.dict["Devin Booker"]!), season: "2017")
+                super.viewDidLoad()
+        season.getPlayer(id: String(playerDict.dict["Alex Abrines"]!), season: "2017", number: 1)
         season.delegate = self
         playerOneSearchTextField.delegate = self
+        playerTwoSearchTextField.delegate = self
     }
     
-    func updateStats(pd: PlayerDisplay) {
+    func updatePlayerOneStats(pd: PlayerDisplay) {
         DispatchQueue.main.async {
             print(pd)
             self.playerOnePointsLabel.text = String(format: "PPG: %.1f", pd.ppg)
@@ -33,11 +42,29 @@ class MainViewController: UIViewController, PlayerDelegate, UITextFieldDelegate 
         }
     }
     
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        print(playerOneSearchTextField.text!)
-        playerOneSearchTextField.endEditing(true)
-        return true
+    func updatePlayerTwoStats(pd: PlayerDisplay) {
+        DispatchQueue.main.async {
+            print(pd)
+            self.playerTwoPointsLabel.text = String(format: "PPG: %.1f", pd.ppg)
+            self.playerTwoAssistsLabel.text = String(format: "APG: %.1f", pd.apg)
+            self.playerTwoReboundsLabel.text = String(format: "RPG: %.1f", pd.rpg)
+            self.playerTwoBlocksLabel.text = String(format: "BPG: %.1f", pd.bpg)
+        }
     }
     
-}
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if playerOneSearchTextField.isFirstResponder {
+            season.getPlayer(id: String(playerDict.dict[playerOneSearchTextField.text!]!), season: "2017", number: 1)
+            playerOneSearchTextField.endEditing(true)
+            return true
+        } else if playerTwoSearchTextField.isFirstResponder {
+            season.getPlayer(id: String(playerDict.dict[playerTwoSearchTextField.text!]!), season: "2017", number: 2)
+            playerTwoSearchTextField.endEditing(true)
+            return true
+    
+        }
+        
+        return false
+    }
 
+}
--- NBA Player Compare/Model/Database.swift
+++ NBA Player Compare/Model/Database.swift
@@ -8,13 +8,14 @@
 import Foundation
 
 protocol PlayerDelegate {
-    func updateStats(pd: PlayerDisplay)
+    func updatePlayerOneStats(pd: PlayerDisplay)
+    func updatePlayerTwoStats(pd: PlayerDisplay)
 }
 
 class Database {
     var delegate: PlayerDelegate?
     
-    func getPlayer(id: String, season: String) {
+    func getPlayer(id: String, season: String, number: Int) {
         let urlString = "https://api-nba-v1.p.rapidapi.com/players/statistics?id=\(id)&season=\(season)"
         print("url string - " + urlString)
         let headers = [
@@ -36,7 +37,7 @@ class Database {
                 let httpResponse = response as? HTTPURLResponse
                 print(httpResponse!)
                 if let safeData = data {
-                    self.parseJSON(playerData: safeData, season: season)
+                    self.parseJSON(playerData: safeData, season: season, number: number)
                 }
             }
         })
@@ -44,18 +45,18 @@ class Database {
     }
     
 
-    func parseJSON(playerData: Data, season: String) {
+    func parseJSON(playerData: Data, season: String, number: Int) {
         let decoder = JSONDecoder()
         do {
             print(String(decoding:playerData, as:UTF8.self))
             let playerGames = try decoder.decode(Player.self, from: playerData)
-            generatePlayerAverage(player: playerGames, season: season)
+            generatePlayerAverage(player: playerGames, season: season, number: number)
         } catch {
             print(error)
         }
     }
     
-    func generatePlayerAverage(player: Player, season : String) {
+    func generatePlayerAverage(player: Player, season : String, number: Int) {
         var firstname: String
         var lastname: String
         var pointTotal: Float = 0.0
@@ -76,7 +77,14 @@ class Database {
             firstname = player.response[0].player.firstname
             lastname = player.response[0].player.lastname
             let pd = PlayerDisplay(firstname: firstname, lastname: lastname, season: season, ppg: pointTotal/gameCount, rpg: rebTotal/gameCount, apg: assistTotal/gameCount, bpg: blockTotal/gameCount)
-            self.delegate?.updateStats(pd: pd)
+            if number == 1 {
+                self.delegate?.updatePlayerOneStats(pd: pd)
+            } else if number == 2 {
+                self.delegate?.updatePlayerTwoStats(pd: pd)
+            } else {
+                print("other number given")
+            }
+      
              
         } else {
             print("error: there are no statistics for this year")
